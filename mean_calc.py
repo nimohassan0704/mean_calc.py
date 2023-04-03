db0fd96cacabcfc89d5a1373e4f3065a3b3a4baa
nimohassan0704 <129746720+nimohassan0704@users.noreply.github.com>
2023-04-03 16:43:52 +0300

    Create mean_calc.py

---
+++ mean_calc.py
@@ -0,0 +1,12 @@
+numbers = [2, 4, 6, 8, 10]
+
+# сумма всех чисел в списке
+sum_of_numbers = sum(numbers)
+
+# количество чисел в списке
+num_of_numbers = len(numbers)
+
+# среднее значение
+mean = sum_of_numbers / num_of_numbers
+
+print("Среднее значение чисел в списке: ", mean)
